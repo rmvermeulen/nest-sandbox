1a95768b93a7adbbece166f301d6057d7172eac8
Rasmus Vermeulen <vermeulen.rasmus@gmail.com>
2018-02-13 13:46:18 +0100

    Added graphql, types-installer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,26 @@
-import { Module } from '@nestjs/common'
+import {
+  Module,
+  MiddlewaresConsumer,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common'
+import { graphqlExpress } from 'apollo-server-express'
+import { GraphQLModule } from '@nestjs/graphql'
 import { HomeController } from './controllers/home/home.controller'
 
 @Module({
   controllers: [HomeController],
+  imports: [GraphQLModule],
 })
-export class ApplicationModule {}
+export class ApplicationModule implements NestModule {
+  configure(consumer: MiddlewaresConsumer) {
+    consumer
+      .apply(
+        graphqlExpress(req => ({
+          schema: {},
+          rootValue: req,
+        })),
+      )
+      .forRoutes({ path: '/graphql', method: RequestMethod.ALL })
+  }
+}
