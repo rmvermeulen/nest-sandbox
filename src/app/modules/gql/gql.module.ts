ffa6f708e11ebe547535a1ffd4eb81e1d50c2908
Rasmus Vermeulen <vermeulen.rasmus@gmail.com>
2018-03-25 14:28:12 +0200

    redirect everything to ide

--- src/app/modules/gql/gql.module.ts
+++ src/app/modules/gql/gql.module.ts
@@ -6,6 +6,7 @@ import {
 } from '@nestjs/common'
 import { GraphQLModule } from '@nestjs/graphql'
 import { graphiqlExpress, graphqlExpress } from 'apollo-server-express'
+import { Request as Req, Response as Res } from 'express'
 import { makeExecutableSchema } from 'graphql-tools'
 
 import { resolvers } from './resolvers'
@@ -28,18 +29,14 @@ export class GQLModule implements NestModule {
           schema: executableSchema,
         })),
       )
-      .forRoutes({
-        method: RequestMethod.ALL,
-        path: '/graphql',
-      })
-      .apply(
-        graphiqlExpress(() => ({
-          endpointURL: '/graphql',
-        })),
-      )
+      .forRoutes({ method: RequestMethod.ALL, path: '/graphql' })
+      .apply(graphiqlExpress(() => ({ endpointURL: '/graphql' })))
       .forRoutes({
         method: RequestMethod.ALL,
         path: '/graphiql',
       })
+      // route everything else to the IDE
+      .apply((req: Req, res: Res) => res.redirect('/graphiql'))
+      .forRoutes({ method: RequestMethod.ALL, path: '*' })
   }
 }
